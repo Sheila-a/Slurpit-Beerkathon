bbf041494c27c3fd7a4df3d8c98828328cd28114
Sheila-a <ugwuodo20@gmail.com>
2023-03-31 08:56:22 +0100

    Feat: User Dashboard Ready

--- src/assets/img.js
+++ src/assets/img.js
@@ -3,5 +3,6 @@ import Blue from "./crd_1.png";
 import Green from "./crd_2.png";
 import Yellow from "./crd_3.png";
 import Pink from "./crd_4.png";
+import Beer from "./Beer.png";
 
-export { Stock, Blue, Green, Yellow, Pink };
+export { Stock, Blue, Green, Yellow, Pink, Beer };
