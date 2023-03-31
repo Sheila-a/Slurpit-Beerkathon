197ca9cb90c4d232d644df0f7ae5a856e15de67e
Ugwuodo Chioma S <sheila.codes.m@gmail.com>
2023-03-31 10:31:02 +0100

    fix deploy error

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
+import { Route, BrowserRouter as Router } from "react-router-dom";
 import Login from "./pages/Login/login";
 import Signup from "./pages/Signup/signup";
 import Admindb from "./pages/AdminDB/admindb";
@@ -10,7 +10,7 @@ function App() {
   return (
     <div className="App">
       <Router>
-        <Switch>
+        <Routes>
           <Route path="/login" element={<Login />} />
           <Route path="/admin_dashboard" element={<Admindb />} />
           <Route path="/user_dashboard" element={<Userdb />} />
@@ -19,7 +19,7 @@ function App() {
           <Route path="/checkout_stock" element={<Checkout />} />
 
           {/* <LSide/> */}
-        </Switch>
+        </Routes>
       </Router>
     </div>
   );
